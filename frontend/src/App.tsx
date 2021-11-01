0da0859bd9df1197114634d3839c83525cd6bfc7
Telmo Ricardo <telmoricardorosa@gmail.com>
2021-11-01 11:40:46 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
+import Datatable from "components/Datatable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá mundo!</h1>
+    <>
+    <NavBar/>
+    <div className="container">
+      <h1 className="text-primary">Olá mundo!</h1>     
+      <Datatable></Datatable> 
     </div>
+    <Footer/>
+    </>
+   
   );
 }
 
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/telmoricardo/" target="_blank" rel="noreferrer">Telmo Ricardo</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/telmoricardo78" target="_blank" rel="noreferrer">@telmoricardo78</a></small></p>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
