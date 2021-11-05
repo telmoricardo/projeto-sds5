d8041d775a69ce4b4c5b90ff64dd9e522668dfef
Telmo Ricardo <telmoricardorosa@gmail.com>
2021-11-05 17:29:27 -0300

    correcao

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -23,26 +23,7 @@ function DonutChart() {
 
                 setChartData({ labels: myLabels, series: mySeries });                
             })
-    }, []);
-
-    //forma errada
-    //let chartData : ChartData = {labels: [], series: []};
-
-    /*axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then(response => {
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x => x.sum);
-
-            //chartData = {labels: myLabels, series: mySeries};
-            setChartData({labels: myLabels, series: mySeries});
-            console.log(chartData);            
-        });*/
-
-    const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }
+    }, []);      
 
     const options = {
         legend: {
